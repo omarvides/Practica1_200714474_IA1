ec09352afedc9c5121a86b99f295a1c2a3a03edf
omarvides <omarvides@gmail.com>
2012-09-20 20:21:41 -0700

    Creacion de proyecto e Interfaz grafica Basica.

---
+++ Proyecto1_IA_2012/src/proyecto1_ia_2012/Proyecto1_IA_2012.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto1_ia_2012;
+
+/**
+ *
+ * @author Omar
+ */
+public class Proyecto1_IA_2012 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
